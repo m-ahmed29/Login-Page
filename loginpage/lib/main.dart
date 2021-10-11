027d958574376efbcc5a8e939e319b892e974465
Muhammad <muhammadahmed1829@gmail.com>
2021-10-12 01:23:25 +0500

    first commit

--- loginpage/lib/main.dart
+++ loginpage/lib/main.dart
@@ -5,79 +5,83 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.green[200],
-            title: Center(
-              child: Text('Login Page'),
-            ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple,
+          title: Center(
+            child: Text('Login Page'),
           ),
-          body: Center(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 50,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                width: 300,
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.email),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      hintText: "Email"),
                 ),
-                Container(
-                  width: 300,
-                  child: TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.email),
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                        hintText: "Email"),
-                  ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: 300,
+                child: TextField(
+                  obscureText: true,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      hintText: "Password"),
                 ),
-                SizedBox(
-                  height: 30,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: 300,
+                child: TextField(
+                  obscureText: true,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      hintText: " Conform Password"),
                 ),
-                Container(
-                  width: 300,
-                  child: TextField(
-                    obscureText: true,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.lock),
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                        hintText: "Password"),
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  width: 300,
-                  child: TextField(
-                    obscureText: true,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.lock),
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                        hintText: " Conform Password"),
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                FlatButton(
-                    color: Colors.green[200],
-                    onPressed: () {},
-                    child: Text('Login'))
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              FlatButton(
+                  color: Colors.purple,
+                  onPressed: () {},
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white),
+                  )),
+              SizedBox(
+                height: 20,
+              ),
+              Text('New user? Sign up here')
+            ],
           ),
-          backgroundColor: Colors.lime[50],
-        ));
+        ),
+      ),
+    );
   }
 }
